97f6b285427ffeaa00d3f5bd25f07df1f2f36177
Gyuwon Yi <gyuwon@live.com>
2024-07-28 18:56:17 +0900

    Spring Boot 앱 초기화

---
+++ spring-boot-app/src/test/java/greeting/GreetingApplicationTests.java
@@ -0,0 +1,12 @@
+package greeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
